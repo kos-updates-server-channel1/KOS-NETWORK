a0eb569248a93af4cb7e785f07ca2a2d0f54a87b
symcity <symcity1001@naver.com>
2017-10-01 19:02:40 +0900

    Just temp2

--- KOSPythonBigDataCrawler/KOSPythonBigDataCrawler.py
+++ KOSPythonBigDataCrawler/KOSPythonBigDataCrawler.py
@@ -1,1 +1,11 @@
-import 
+def get_html(url):
+   _html = ""
+   resp = requests.get(url)
+   if resp.status_code == 200:
+      _html = resp.text
+   return _html
+
+from bs4 import BeautifulSoup
+URL = "https://kos623.wordpress.com/"
+html = get_html(URL)
+soup = BeautifulSoup(html, 'html.parser')
